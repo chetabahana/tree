f6a2043e6bbd939095a1327bd8cecf39a5bbce4f
Chetabahana <chetabahana@gmail.com>
2019-11-14 06:27:58 +0700

    Update scalars.js

--- test/fixtures/three.graphql/schema/scalars.js
+++ test/fixtures/three.graphql/schema/scalars.js
@@ -2,8 +2,7 @@
 
 /**
  * Color type
- */
-
+ 
 exports.Color = {
   parseValue(value) {
     return parseInt(String(value))
@@ -13,3 +12,28 @@ exports.Color = {
     return parseInt(String(value))
   }
 }
+*/
+
+import { GraphQLScalarType } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
+exports.Color = new GraphQLScalarType({
+  name: 'Color',
+  description: 'Description of my custom scalar type',
+  serialize(value) {
+    let result;
+    // Implement your own behavior here by setting the 'result' variable
+    return result;
+  },
+  parseValue(value) {
+    let result;
+    // Implement your own behavior here by setting the 'result' variable
+    return result;
+  },
+  parseLiteral(ast) {
+    switch (ast.kind) {
+      // Implement your own behavior here by returning what suits your needs
+      // depending on ast.kind
+    }
+  }
+});
