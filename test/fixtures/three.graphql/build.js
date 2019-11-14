afc72436da8e5588088b5fa1158024541c296ad7
chetabahana <chetabahana@gmail.com>
2019-11-14 13:07:45 +0700

    Add Support for CI/CD Workflows

--- test/fixtures/three.graphql/build.js
+++ test/fixtures/three.graphql/build.js
@@ -116950,6 +116950,26 @@ exports.Color = {
 }
 */
 
+exports.Color = new GraphQLScalarType({
+  name: 'Color',
+  description: 'Description of my custom scalar type',
+  serialize(value) {
+    let result;
+    // Implement your own behavior here by setting the 'result' variable
+    return result;
+  },
+  parseValue(value) {
+    let result;
+    // Implement your own behavior here by setting the 'result' variable
+    return result;
+  },
+  parseLiteral(ast) {
+    switch (ast.kind) {
+      // Implement your own behavior here by returning what suits your needs
+      // depending on ast.kind
+    }
+  }
+});
 },{}],484:[function(require,module,exports){
 'use strict'
 
