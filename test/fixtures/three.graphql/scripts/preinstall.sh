fb0732fa6a4862810adf656efe8ca93b51242c24
Chetabahana <chetabahana@gmail.com>
2019-11-15 05:07:20 +0700

    Update preinstall.sh

--- test/fixtures/three.graphql/scripts/preinstall.sh
+++ test/fixtures/three.graphql/scripts/preinstall.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Update npm to the latest
-echo -e "\n$hr\nUPDATE\n$hr"
-sudo npm install npm@latest -g
-
 # Delete all regenerated files
 rm -rf graphiql.css package-lock.json yarn.lock
