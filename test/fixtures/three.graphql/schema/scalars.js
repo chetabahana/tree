77f81327f56ca4dea8d280a1e634f68116b6409d
Chetabahana <chetabahana@gmail.com>
2019-11-14 12:29:35 +0700

    Color type

--- test/fixtures/three.graphql/schema/scalars.js
+++ test/fixtures/three.graphql/schema/scalars.js
@@ -13,27 +13,3 @@ exports.Color = {
   }
 }
 */
-
-import { GraphQLScalarType } from 'graphql';
-import { makeExecutableSchema } from 'graphql-tools';
-
-const Color = new GraphQLScalarType({
-  name: 'Color',
-  description: 'Description of my custom scalar type',
-  serialize(value) {
-    let result;
-    // Implement your own behavior here by setting the 'result' variable
-    return result;
-  },
-  parseValue(value) {
-    let result;
-    // Implement your own behavior here by setting the 'result' variable
-    return result;
-  },
-  parseLiteral(ast) {
-    switch (ast.kind) {
-      // Implement your own behavior here by returning what suits your needs
-      // depending on ast.kind
-    }
-  }
-});
