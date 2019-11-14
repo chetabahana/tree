7f35ca77dcf0a01ec96ed5d65a7ff884a1e16e81
Chetabahana <chetabahana@gmail.com>
2019-11-15 00:08:11 +0700

    Update install.sh

--- test/fixtures/three.graphql/install.sh
+++ test/fixtures/three.graphql/install.sh
@@ -6,9 +6,5 @@ find node_modules -name graphql
 grep version node_modules/graphql/package.json
 rm -rf node_modules/graph.ql/node_modules/graphql
 
-# Resolving: TypeError
-SED_ERROR='throw new TypeError("Int cannot represent non-integer value: ".concat((0, _inspect.default)(value)))'
-sed -i "s/$SED_ERROR/return null/g" build.js
-
 # Copy graphiql.css to current directory
 cp -f node_modules/graphiql/graphiql.css .
