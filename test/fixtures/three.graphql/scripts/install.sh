e8516b82f46a747e790ad87630f5e99756b34d98
Chetabahana <chetabahana@gmail.com>
2019-11-15 22:08:01 +0700

    echo -e "\n$hr\nAUDIT\n$hr"

--- test/fixtures/three.graphql/scripts/install.sh
+++ test/fixtures/three.graphql/scripts/install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo -e "\n$hr\nVERSION\n$hr"
-# Resolving: Cannot use GraphQLSchema "[object Object]" from another module or realm.
-# https://github.com/apollographql/react-apollo/issues/742#issuecomment-338366662
-find node_modules -name graphql
-grep version node_modules/graphql/package.json
-rm -rf node_modules/graph.ql/node_modules/graphql
-
 # Copy graphiql.css to current directory
 cp -f node_modules/graphiql/graphiql.css .
+
+echo -e "\n$hr\nAUDIT\n$hr"
