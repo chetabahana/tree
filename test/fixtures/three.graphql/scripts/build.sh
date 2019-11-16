93589acb59003e871c1814ac02ad4449b0c65190
Chetabahana <chetabahana@gmail.com>
2019-11-16 18:53:39 +0700

    Update build.sh

--- test/fixtures/three.graphql/scripts/build.sh
+++ test/fixtures/three.graphql/scripts/build.sh
@@ -3,7 +3,7 @@
 # Run browserify
 browserify editor.js -t brfs -o build.js
 
-# Resolving: TypeError
+# Resolving: TypeError Int cannot represent non-integer value
 SED_ERROR='throw new TypeError("\Int cannot represent non-integer value: ".concat((0, _inspect.default)(value)))'
 sed -i "s/$SED_ERROR/return null/g" build.js
 
