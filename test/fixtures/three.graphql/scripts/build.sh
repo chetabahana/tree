cb9efd1d8f53034e11c93c581d9d6cb7cd4af009
Chetabahana <chetabahana@gmail.com>
2019-11-16 18:52:22 +0700

    Update build.sh

--- test/fixtures/three.graphql/scripts/build.sh
+++ test/fixtures/three.graphql/scripts/build.sh
@@ -6,3 +6,7 @@ browserify editor.js -t brfs -o build.js
 # Resolving: TypeError
 SED_ERROR='throw new TypeError("\Int cannot represent non-integer value: ".concat((0, _inspect.default)(value)))'
 sed -i "s/$SED_ERROR/return null/g" build.js
+
+# Resolving: Variable "$width" got invalid value NaN; Expected type Float. Float cannot represent non numeric value: NaN
+SED_ERROR='throw new TypeError("Float cannot represent non numeric value: ".concat((0, _inspect.default)(value)))'
+sed -i "s/$SED_ERROR/return null/g" build.js
