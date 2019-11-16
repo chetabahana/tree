c000d73710d92bbdc2aeef06601fcc9063337b16
Chetabahana <chetabahana@gmail.com>
2019-11-16 22:29:02 +0700

    Update index.js

--- test/fixtures/three.graphql/schema/index.js
+++ test/fixtures/three.graphql/schema/index.js
@@ -4,7 +4,7 @@
  * Module dependendies.
  */
 
-const createSchema = require('graphql')
+const buildSchema = require('graphql')
 const path = require('path')
 const fs = require('fs')
 
@@ -106,4 +106,4 @@ const implementations = Object
 .map((key) => map[key])
 .reduce((i, type) => Object.assign(i, type.implementation), {})
 
-module.exports = createSchema(String(buffer), implementations).schema
+module.exports = buildSchema(String(buffer), implementations).schema
