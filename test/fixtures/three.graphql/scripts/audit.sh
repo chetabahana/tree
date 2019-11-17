b580029c0f6f8d6d55c7c20f67fdee8afe2a7317
Chetabahana <chetabahana@gmail.com>
2019-11-17 13:53:13 +0700

    version": "0.1.3

--- test/fixtures/three.graphql/scripts/audit.sh
+++ test/fixtures/three.graphql/scripts/audit.sh
@@ -2,12 +2,3 @@
 
 # Run audit
 npm audit fix
-
-echo -e "\n$hr\nRESOLVE VERSION\n$hr"
-# Resolving: Cannot use GraphQLSchema "[object Object]" from another module or realm.
-# https://github.com/matthewmueller/graph.ql/blob/master/package.json
-# https://github.com/apollographql/react-apollo/issues/742#issuecomment-338366662
-echo Resolving a duplicate version of graphql and use one version only:
-find node_modules -name graphql
-grep version node_modules/graphql/package.json
-rm -rf node_modules/graph.ql/node_modules/graphql
