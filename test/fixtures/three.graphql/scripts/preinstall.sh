9dfd37c9dad5461232cdbe026051bfc9fe5fb876
Chetabahana <chetabahana@gmail.com>
2019-11-19 13:25:11 +0700

    npm install --save-dev babelify @babel/core

--- test/fixtures/three.graphql/scripts/preinstall.sh
+++ test/fixtures/three.graphql/scripts/preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update npm to the latest
 sudo npm install npm@latest -g
-npm install --save-dev babelify @babel/core
+
 # Delete all regenerated files
 rm -rf graphiql.css package-lock.json yarn.lock
 echo -e "\n$hr\nINSTALL\n$hr"
