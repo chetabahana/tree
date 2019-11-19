ecdb6c7bf3b18ed18e23f256c6c8368689cae713
Chetabahana <chetabahana@gmail.com>
2019-11-19 23:54:59 +0700

    Update audit.sh

--- test/fixtures/three.graphql/scripts/audit.sh
+++ test/fixtures/three.graphql/scripts/audit.sh
@@ -11,21 +11,13 @@ echo -e "\n$hr\nFORK VERSION\n$hr"
 # Resolving: Cannot use GraphQLSchema "[object Object]" from another module or realm.
 # https://github.com/apollographql/react-apollo/issues/742#issuecomment-338366662
 # https://github.com/matthewmueller/graph.ql/blob/master/package.json
-find node_modules -name graphql
-grep version node_modules/graphql/package.json
 rm -rf node_modules/graph.ql/node_modules/graphql
 
 # https://stackoverflow.com/a/56515445/4058484
 # https://stackoverflow.com/a/34246579/4058484
 npm list | grep "graph.ql"
-
-# https://stackoverflow.com/a/47040266/4058484
-npm view graph.ql dependencies
-npx howfat -r tree graph.ql
-
-# https://stackoverflow.com/a/26005786/4058484
-npm install -g npm-remote-ls
-npm-remote-ls graph.ql
+find node_modules -name graphql
+grep version node_modules/graphql/package.json
 
 echo -e "\n$hr\nROOT VERSION\n$hr"
 # https://stackoverflow.com/a/37211072/4058484
