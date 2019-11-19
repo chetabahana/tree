a6d3db0732c52aa5dcf217d43e77250d3c602834
Chetabahana <chetabahana@gmail.com>
2019-11-19 19:34:55 +0700

    Update build.sh

--- test/fixtures/three.graphql/scripts/build.sh
+++ test/fixtures/three.graphql/scripts/build.sh
@@ -3,7 +3,7 @@
 # Run browserify
 browserify editor.js -t brfs -o build.js --no-babelrc
 
-# Resolving: TypeError Int cannot represent non-integer value
+# Resolving: Error/Warning
 FIND='throw new TypeError("\Int cannot represent non-integer value: ".concat((0, _inspect.default)(value)))'
 REPLACE='return null'
 sed -i "s/$FIND/$REPLACE/g" build.js
