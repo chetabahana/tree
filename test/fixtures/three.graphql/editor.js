be9af0ab270c4020a1980869f4205c8740337ca6
Chetabahana <chetabahana@gmail.com>
2019-11-19 11:25:50 +0700

    Update editor.js

--- test/fixtures/three.graphql/editor.js
+++ test/fixtures/three.graphql/editor.js
@@ -6,66 +6,11 @@ const ReactDOM = require('react-dom')
 const React = require('react')
 const ready = require('domready')
 const vm = require('vm')
+const kDefaultQuery = ``
 
 let query
 let currentQuerySource = null
 
-const kDefaultQuery = `
-## Renders a scene into a viewport with
-## a PerspectiveCamera and a Scene containing
-## a Mesh constructed with a BoxGeometry and
-## a MeshBasicMaterial
-query render($width: Float,
-             $height: Float,
-             #$rotateX: Float,
-             #$rotateY: Float
-             $tickr: Float) {
-  ## Describes renderer
-  WebGLRenderer {
-    ## set viewport width/height based on context inputs
-    setSize(width: $width, height: $height)
-
-    ## Configures a PerspectiveCamera
-    PerspectiveCamera(fov: 75, aspect: 1, near: 1, far: 10000) {
-      setPosition(z: 500)
-    }
-
-    ## Describes a Scene
-    Scene {
-
-      ## Describes a named Mesh
-      a: Mesh(name: "box-a") {
-        setRotation(x: $tickr)
-        setPosition(x: 200, y: 200)
-        ...BoxWireframe
-      }
-
-      b: Mesh(name: "box-b") {
-        setPosition(x: -200, y: -200)
-        setRotation(y: $tickr)
-        ...Box
-      }
-
-      c: Mesh(name: "box-c") {
-        setRotation(z: $tickr)
-        setPosition(x: 0)
-        ...Box
-      }
-    }
-  }
-}
-
-fragment BoxWireframe on Mesh {
-  ## Contruct geometry and material for mesh
-  BoxGeometry(width: 100, height: 100, depth: 100)
-  MeshBasicMaterial(wireframe: true)
-}
-
-fragment Box on Mesh {
-  BoxGeometry(width: 100, height: 100, depth: 100)
-  MeshBasicMaterial(wireframe: false)
-}
-`
 
 ready(() => {
   const canvas = document.querySelector('#viewport canvas')
