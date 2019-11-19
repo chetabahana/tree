819932fef205888c22a7308b68aef48d0bcd61fd
Chetabahana <chetabahana@gmail.com>
2019-11-19 19:52:30 +0700

    Update build.sh

--- test/fixtures/three.graphql/scripts/build.sh
+++ test/fixtures/three.graphql/scripts/build.sh
@@ -8,7 +8,7 @@ FIND='throw new TypeError("\Int cannot represent non-integer value: ".concat((0,
 REPLACE='return null'
 sed -i "s/$FIND/$REPLACE/g" build.js
 
-FIND="warning$1(false, 'Encountered two children with the same key, `%s`. ' + 'Keys should be unique so that components maintain their identity ' + 'across updates. Non-unique keys may cause children to be ' + 'duplicated and/or omitted — the behavior is unsupported and ' + 'could change in a future version.', key);"
+FIND="// warning$1(false, 'Encountered two children with the same key,"
 REPLACE=""
 sed -i "s/$FIND/$REPLACE/g" build.js
 
