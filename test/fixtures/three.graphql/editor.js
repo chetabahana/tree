0d8fe1cbc1691d65913dfc37754a87c15893f3ec
Chetabahana <chetabahana@gmail.com>
2019-11-21 15:35:45 +0700

    Update editor.js

--- test/fixtures/three.graphql/editor.js
+++ test/fixtures/three.graphql/editor.js
@@ -129,3 +129,10 @@ ready(() => {
     }
   }
 })
+
+(function () {
+    var _render = ReactDOM.render;
+    ReactDOM.render = function () {
+        return arguments[1].react = _render.apply(this, arguments);
+    };
+})();
