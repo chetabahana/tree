52288373a24848927e48409501195f33d6f6441d
Chetabahana <chetabahana@gmail.com>
2019-12-18 00:29:34 +0700

    Update webpack.config.js

--- test/fixtures/three.graphql/webpack.config.js
+++ test/fixtures/three.graphql/webpack.config.js
@@ -2,32 +2,5 @@ const path = require('path');
 module.exports = {
   target: 'node',
   mode: 'production',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: [':data-src']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
+  devtool: 'source-map'
 };
